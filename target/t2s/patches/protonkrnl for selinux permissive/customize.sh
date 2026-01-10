8f53b7d151a63e93fa4d02edcf78aa71dac945af
ditternation <ditternation@outlook.com>
2026-01-10 16:27:49 +0100

    MonsterROM: adding proton permissive kernel

---
+++ target/t2s/patches/protonkrnl for selinux permissive/customize.sh
@@ -0,0 +1,3 @@
+cp -fa "$SRC_DIR/target/t2s/patches/protonkrnl for selinux permissive/img/boot.img" "$WORK_DIR/kernel/boot.img"
+cp -fa "$SRC_DIR/target/t2s/patches/protonkrnl for selinux permissive/img/vendor_boot.img" "$WORK_DIR/kernel/vendor_boot.img"
+
---
+++ unica/mods/paradigm/customize.sh
@@ -0,0 +1,15 @@
+# Set AI Version to 20263 (latest)
+SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_COMMON_CONFIG_AI_VERSION" "20263"
+
+# Spoofing S26 Ultra
+# System
+SET_PROP "system" "ro.product.system.model" "SM-S948B"
+
+# System_ext
+SET_PROP "system_ext" "ro.product.system_ext.model" "SM-S948B"
+
+# Product
+SET_PROP "product" "ro.product.product.model" "SM-S948B"
+
+# ODM
+SET_PROP "odm" "ro.product.odm.model" "SM-S948B"
