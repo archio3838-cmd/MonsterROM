e60fae47d12fa9243603ba162f5325a0e56c8db4
archio3838-cmd <archio3838@gmail.com>
2026-01-16 09:41:41 +0300

    Update customize.sh

--- unica/patches/selinux/customize.sh
+++ unica/patches/selinux/customize.sh
@@ -1,18 +1,11 @@
-# UN1CA SELinux entries removal list
-# - Append new type entries to the ENTRIES list
-# - Add the EXACT type entry, DO NOT just add a common pattern (eg. "fabriccrypto", "fabriccrypto_exec" and NOT just "fabriccrypto")
-# - DO NOT add the API version at the end of the entry (eg. "fabriccrypto" and NOT "fabriccrypto_30_0")
-# - DO NOT add any parenthesis or statements (eg. "fabriccrypto" and NOT "expanttypeattribute ... (fabriccrypto)")
-# - DO NOT add unnecessary types or remove the existing ones unless they aren't necessary anymore for all devices
+# UN1CA SELinux entries removal list (VENDORLESS)
+# Cleans ONLY system_ext and plat sepolicy
+# Vendor is NOT present and NOT referenced
 
 # One UI 8.0 additions
 ENTRIES+="
 heatmap_default
 heatmap_default_exec
-"
-
-DUPLICATES+="
-init.svc.vendor.wvkprov_server_hal
 "
 
 # One UI 7.0 additions
@@ -45,59 +38,46 @@ hal_dsms_service
 uwb_regulation_skip_prop
 "
 
-# [
 GET_SYSTEM_EXT()
 {
-    if $TARGET_OS_BUILD_SYSTEM_EXT_PARTITION; then
+    if $TARGET_HAS_SYSTEM_EXT; then
         echo "system_ext"
     else
         echo "system/system/system_ext"
     fi
 }
 
-CIL_NAME="$(head -n 1 "$WORK_DIR/vendor/etc/selinux/plat_sepolicy_vers.txt")"
-
-VENDOR_API_LIST="$(find "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping" -type f -printf "%f\n" | \
-                    sed '/.compat./d' | sed 's/.cil//' | sed 's/\./_/' | sort)"
-# ]
+# Detect current platform mapping CIL
+CIL_NAME="33.0"
+API_LIST="$(ls "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping" \
+            | sed '/.compat./d' | sed 's/.cil//' | sed 's/\./_/' | sort)"
 
 for e in $ENTRIES; do
-    if grep -q -F "($e)" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping/$CIL_NAME.cil" || \
-         grep -q -F "${e}_${CIL_NAME//./_}" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping/$CIL_NAME.cil"; then
-        # the problematic entry is currently present in system_ext, check if we need to remove it
-        if ! grep -q -F "(type $e)" "$WORK_DIR/vendor/etc/selinux/plat_pub_versioned.cil"; then
-            # the problematic entry is not supported by the target device
-            LOG "- \"$e\" SELinux entry not supported. Removing"
-            sed -i "/($e)/d" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping/$CIL_NAME.cil"
-            for a in $VENDOR_API_LIST; do
-                sed -i "/${e}_${a}/d" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping/$CIL_NAME.cil"
-            done
-            if grep -q "genfscon.*$e" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/system_ext_sepolicy.cil"; then
-                sed -i "/genfscon.*$e/d" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/system_ext_sepolicy.cil"
-            fi
-            if grep -q "genfscon.*$e" "$WORK_DIR/system/system/etc/selinux/plat_sepolicy.cil"; then
-                sed -i "/genfscon.*$e/d" "$WORK_DIR/system/system/etc/selinux/plat_sepolicy.cil"
-            fi
-        fi
+    LOG "- Removing \"$e\" from system_ext"
+
+    sed -i "/($e)/d" \
+        "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping/$CIL_NAME.cil"
+
+    for a in $API_LIST; do
+        sed -i "/${e}_${a}/d" \
+            "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping/$CIL_NAME.cil"
+    done
+
+    if [ -f "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/system_ext_sepolicy.cil" ]; then
+        sed -i "/genfscon.*$e/d" \
+            "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/system_ext_sepolicy.cil"
+    fi
+
+    if [ -f "$WORK_DIR/system/system/etc/selinux/plat_sepolicy.cil" ]; then
+        sed -i "/genfscon.*$e/d" \
+            "$WORK_DIR/system/system/etc/selinux/plat_sepolicy.cil"
     fi
 done
 
-for e in $DUPLICATES; do
-    if grep -q "^$e.*" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/system_ext_property_contexts"; then
-        # the problematic entry is currently present in system_ext, check if we need to remove it
-        if grep -q "^$e.*" "$WORK_DIR/vendor/etc/selinux/vendor_property_contexts"; then
-            # the problematic entry is found in target vendor
-            LOG "- \"$e\" SELinux duplicate entry found. Removing"
-            sed -i "s/^$e/#SEC_DUPLICATE: $e/g" "$WORK_DIR/vendor/etc/selinux/vendor_property_contexts"
-        fi
-    fi
-done
+LOG_STEP_IN "- Apply genfscon rules fix to plat_sepolicy.cil"
 
-
-LOG_STEP_IN "- Apply genconfsrulesfix to platsepolicy.cil"
-# Delete specific genfscon lines
 PLAT_SEPOLICY="$WORK_DIR/system/system/etc/selinux/plat_sepolicy.cil"
-sed -i "$PLAT_SEPOLICY" \
+[ -f "$PLAT_SEPOLICY" ] && sed -i "$PLAT_SEPOLICY" \
     -e '\#(genfscon bpf "/cputimeinstate" (u object_r fs_bpf_cputimeinstate ((s0) (s0))))#d' \
     -e '\#(genfscon proc "/sys/vm/dirty_writeback_centisecs" (u object_r proc_dirty ((s0) (s0))))#d' \
     -e '\#(genfscon proc "/sys/kernel/firmware_config" (u object_r proc_firmware_config ((s0) (s0))))#d' \
@@ -107,7 +87,8 @@ sed -i "$PLAT_SEPOLICY" \
     -e '\#(genfscon sysfs "/kernel/btf" (u object_r sysfs_btf ((s0) (s0))))#d' \
     -e '\#(genfscon tracefs "/events/f2fs/f2fs_set_page_dirty/" (u object_r debugfs_tracing ((s0) (s0))))#d' \
     -e '\#(genfscon tracefs "/hypervisor" (u object_r debugfs_tracing ((s0) (s0))))#d'
+
 LOG_STEP_OUT
 
-unset ENTRIES DUPLICATES CIL_NAME VENDOR_API_LIST
+unset ENTRIES CIL_NAME API_LIST
 unset -f GET_SYSTEM_EXT
